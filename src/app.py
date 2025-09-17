e6b7bf1e50277659c8eeee25cce7a814f2fc7ca7
Nishanta Banik <nishanta.banik2016@gmail.com>
2025-09-17 16:01:56 +0200

    Adding python-app with K8s deplyments

---
+++ src/app.py
@@ -0,0 +1,41 @@
+from flask import Flask, jsonify
+import datetime, socket
+
+app = Flask(__name__)
+
+@app.route('/api/v1/details')
+def details():
+    return jsonify({
+        'message': 'Hello World',
+        'time': datetime.datetime.now().strftime("%I:%M%:%S%p on %B %d, %Y"),
+        'hostname': socket.gethostname()
+        })
+
+@app.route('/api/v1/healthz')
+
+def health():
+    return jsonify({
+        'status': 'It is UP',
+        'time': datetime.datetime.now().strftime("%I:%M%:%S%p on %B %d, %Y"),
+        'hostname': socket.gethostname()
+        })
+
+if __name__ == '__main__':
+    app.run(debug=True, host="0.0.0.0")
+
+# '/api/v1/details'
+# '/api/v1/healthz'
+
+# from flask import Flask, jsonify
+# import socket
+
+# app = Flask(__name__)
+
+# @app.route('/api/v1/details')
+# def details():
+#     return jsonify({
+#         'hostname': socket.gethostname()
+#     })
+    
+# if __name__=='__main__':
+#     app.run(debug=True)
