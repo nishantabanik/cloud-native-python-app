71d6ae850d743a9f09279888607cbc8177677d40
Nishanta Banik <nishanta.banik2016@gmail.com>
2025-09-18 17:06:15 +0200

    Adding new info endpoint

--- src/app.py
+++ src/app.py
@@ -3,41 +3,25 @@ import datetime, socket
 
 app = Flask(__name__)
 
-@app.route('/api/v1/details')
-def details():
+@app.route('/api/v1/info')
+def info():
     return jsonify({
         'message': 'Hello World',
         'time': datetime.datetime.now().strftime("%I:%M%:%S%p on %B %d, %Y"),
         'hostname': socket.gethostname(),
-        'message' : 'You are doing a great job, my friend!!! :)'
+        'message' : 'You are doing a great job, my friend!!! :)',
+        'deployed_on': 'kubernetes'
         })
 
 @app.route('/api/v1/healthz')
 
 def health():
     return jsonify({
-        'status': 'It is UP',
+        'status': 'It is UP inside Kubernetes',
         'time': datetime.datetime.now().strftime("%I:%M%:%S%p on %B %d, %Y"),
         'hostname': socket.gethostname(),
-        'message' : 'You are doing an AWESOME job, my friend!!'
+        'message' : 'TODAY is going to be an AWESOME, my friend!!'
         })
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0")
-
-# '/api/v1/details'
-# '/api/v1/healthz'
-
-# from flask import Flask, jsonify
-# import socket
-
-# app = Flask(__name__)
-
-# @app.route('/api/v1/details')
-# def details():
-#     return jsonify({
-#         'hostname': socket.gethostname()
-#     })
-    
-# if __name__=='__main__':
-#     app.run(debug=True)
